639f881c7e95482d4c56d18aebba95c418c84a7e
Victor Danila <vvdanila87@gmail.com>
2019-11-16 12:02:56 +0200

    Add logic to thermometer class methods

--- thermometer.py
+++ thermometer.py
@@ -1,16 +1,21 @@
 class Thermometer():
 
-	def __init__(self):
-		pass
+    def __init__(self, temperature):
+        self.temperature = temperature
 
-	def get_temperature(self):
-		pass
+    def get_temperature(self, unit="C"):
+        if unit == "C":
+            return self.temperature
+        elif unit == "F":
+            return self.to_fahrenheit(self.temperature)
 
-	def set_temperature(self):
-		pass
+    def set_temperature(self, temperature):
+        self.temperature = temperature
 
-	def to_celsius(self):
-		pass
+    def to_celsius(self, temperature):
+        temperature = (temperature - 32) * 5 / 9
+        return temperature
 
-	def to_fahrenheit(self):
-		pass
+    def to_fahrenheit(self, temperature):
+        temperature = temperature * 9 / 5 + 32
+        return temperature
